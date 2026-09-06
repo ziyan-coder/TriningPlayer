8d5731c6bd4e751c43bea0aa5b2c85f737ba96a8
ziyan-coder <1610357088@qq.com>
2026-09-06 23:05:41 +0800

    Create CreateTemplateActivity.kt

---
+++ app/src/main/java/com/example/trainingplayer/CreateTemplateActivity.kt
@@ -0,0 +1,10 @@
+package com.example.trainingplayer
+
+import android.app.AlertDialog
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.DocumentsContract
+import android.provider.MediaStore
+import android
